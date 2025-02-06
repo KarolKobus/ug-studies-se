e7646fe2e450eecbfb3c2db8201cdf72bc911af9
Michał Wroński <wronski.mw@gmail.com>
2025-02-06 14:52:36 +0100

    Merge pull request #1 from KarolKobus/gui
    Dodano funkcję wyświetlania raportu końcowego.

--- gui/question3.py
+++ gui/question3.py
@@ -1,19 +1,22 @@
 import tkinter as tk
 from gui.question4 import Question4
 
-def previous_window(root):
+
+def previous_window(root, responses):
     """Cofa do poprzedniego pytania"""
     root.destroy()
     from gui.question2 import Question2  # Opóźniony import (rozwiązuje circular import)
     new_root = tk.Tk()
-    app = Question2(new_root)
+    app = Question2(new_root, responses)
     new_root.mainloop()
 
+
 class Question3:
-    def __init__(self, root):
+    def __init__(self, root, responses):
         self.root = root
         self.root.title("Alzheimer Predictor")
         self.root.geometry("600x800")
+        self.responses = responses  # Przechowywanie odpowiedzi z poprzedniego pytania
 
         # Tytuł sekcji
         tk.Label(root, text="Historia medyczna", font=("Arial", 16, "bold")).pack(pady=10)
@@ -40,14 +43,17 @@ class Question3:
         button_frame = tk.Frame(root)
         button_frame.pack(pady=20)
 
-        tk.Button(button_frame, text="Wstecz", font=("Arial", 14), command=lambda: previous_window(self.root)).pack(side="left", padx=10)
-        tk.Button(button_frame, text="Dalej", font=("Arial", 14), command=self.next_question).pack(side="right", padx=10)
+        tk.Button(button_frame, text="Wstecz", font=("Arial", 14),
+                  command=lambda: previous_window(self.root, self.responses)).pack(side="left", padx=10)
+        tk.Button(button_frame, text="Dalej", font=("Arial", 14), command=self.next_question).pack(side="right",
+                                                                                                   padx=10)
 
     def next_question(self):
-        responses = {q: v.get() for q, v in self.answers.items()}
-        print(responses)  # Można zapisać te wartości lub przekazać dalej
+        for q, v in self.answers.items():
+            self.responses[q] = v.get()
+
         self.root.destroy()
         # Otwórz kolejne pytanie
         root = tk.Tk()
-        app = Question4(root)
+        app = Question4(root, self.responses)
         root.mainloop()
